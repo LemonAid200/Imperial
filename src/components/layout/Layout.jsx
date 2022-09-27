75c679cde6b9047f80f1c9c26b4be9a9e3f11a39
Vsevolod <vsevolodehe@mail.ru>
2022-09-27 21:19:14 +0500

    Clearing vscode on msi

--- src/components/layout/Layout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Layout = ({children}) => {
-  return (
-    <div className="py-10 bg-slate-900 min-h-screen h-full text-white">{children}</div>
-  )
-}
-
-export default Layout

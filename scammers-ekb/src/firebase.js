f47bcf4ed5ca00f613c15677654fa13c1c079875
Vsevolod <vsevolodehe@mail.ru>
2022-12-20 20:09:24 +0500

    vue scammers init

---
+++ scammers-ekb/src/firebase.js
@@ -0,0 +1,22 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBstD17pGVVLljSstodxElT3t6_pzmDfDI",
+//   authDomain: "personaldictonary.firebaseapp.com",
+//   databaseURL: "https://personaldictonary-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "personaldictonary",
+//   storageBucket: "personaldictonary.appspot.com",
+//   messagingSenderId: "687598154975",
+//   appId: "1:687598154975:web:bbc4f8783043826073656a",
+//   measurementId: "G-WPC609DVHS"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
